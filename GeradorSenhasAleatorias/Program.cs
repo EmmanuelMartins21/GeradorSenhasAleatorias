bbdc4ac4c1bb47e00f54d6b19533de6bb6a6a6a8
Emmanuel Martins <emmanuelkta6@gmail.com>
2022-11-07 19:07:39 -0300

    Adicionar arquivos de projeto.

---
+++ GeradorSenhasAleatorias/Program.cs
@@ -0,0 +1,11 @@
+﻿string alfaNumerico = "0123456789!@#$%&*()[]+-/|.abcdefghjkmnpqrstuvwxyz";
+string senhaGerada = null;
+
+
+Random random = new Random();
+for (int i = 0; i <= 10; i++)
+{
+    senhaGerada += alfaNumerico.Substring(random.Next(0, alfaNumerico.Length - 1), 1);
+}
+Console.WriteLine("Sua senhha é:\n");
+Console.WriteLine(senhaGerada);
